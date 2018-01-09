59aed60c32fe8af3052043c2bbc830f92f72a24f
Stuart Auld <sauld@cozero.com.au>
2018-01-10 10:54:52 +1100

    added in the template provider

--- hello/hello.go
+++ hello/hello.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform/version"
 	"github.com/terraform-providers/terraform-provider-aws/aws"
+	"github.com/terraform-providers/terraform-provider-template/template"
 )
 
 func dumpProvider() {
@@ -17,5 +18,6 @@ Welcome! You are using the following packages:-
 
 hashicorp/terraform %s
 terraform-providers/terraform-provider-aws
+terraform-providers/terraform-provider-template
 `, version.VersionString())
 }
