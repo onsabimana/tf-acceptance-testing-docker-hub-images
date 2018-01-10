21ce5f368a2b4435aefaf395c908b7cdff5efad5
Stuart Auld <sauld@cozero.com.au>
2018-01-10 11:32:11 +1100

    tf 0.11.2

---
+++ hello/hello.go
@@ -0,0 +1,19 @@
+package hello
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/version"
+	_ "github.com/terraform-providers/terraform-provider-aws/aws"
+	_ "github.com/terraform-providers/terraform-provider-template/template"
+)
+
+func main() {
+	fmt.Printf(`
+Welcome! You are using the following packages:-
+
+hashicorp/terraform %s
+terraform-providers/terraform-provider-aws
+terraform-providers/terraform-provider-template
+`, version.VersionString())
+}
