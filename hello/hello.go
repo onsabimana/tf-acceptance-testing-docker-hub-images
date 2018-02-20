dd6531d7fee839496fa38e039d278cfa2e888bbc
Stuart Auld <sauld@cozero.com.au>
2018-02-21 08:31:52 +1100

    include the archive provider

--- hello/hello.go
+++ hello/hello.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/terraform/version"
+	_ "github.com/terraform-providers/terraform-provider-archive/archive"
 	_ "github.com/terraform-providers/terraform-provider-aws/aws"
 	_ "github.com/terraform-providers/terraform-provider-pagerduty/pagerduty"
 	_ "github.com/terraform-providers/terraform-provider-template/template"
@@ -14,6 +15,7 @@ func main() {
 Welcome! You are using the following packages:-
 
 hashicorp/terraform %s
+terraform-providers/terraform-provider-archive
 terraform-providers/terraform-provider-aws
 terraform-providers/terraform-provider-pagerduty
 terraform-providers/terraform-provider-template
